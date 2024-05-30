286b4c7d57da237fa4c9896cc289084888010338
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2024-05-30 15:39:28 +0400

    Added starting

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -1,1 +1,60 @@
-﻿
+﻿namespace NumberGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Number Guess Game!");
+            Console.WriteLine("Please tell me ranges where I should look for your number.");
+
+            int min = 0, max = 0;
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.Write("Enter the minimum number: ");
+                try
+                {
+                    min = int.Parse(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+
+            validInput = false; // Reset for the next input
+
+            while (!validInput)
+            {
+                Console.Write("Enter the maximum number: ");
+                try
+                {
+                    max = int.Parse(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+
+            int guess = MakeGuess(min, max);
+            Console.WriteLine($"Your number is {guess}.");
+        }
+
+        static int MakeGuess(int min, int max)
+        {
+            if (min > max)
+            {
+                Console.WriteLine("Range is Invalid. Please enter a valid range.");
+                return -1;
+            }
+            else
+            {
+                return min;
+            }
+        }
+    }
+}
--- NumberGuessGame/obj/Debug/net8.0/NumberGuessGame.AssemblyInfo.cs
+++ NumberGuessGame/obj/Debug/net8.0/NumberGuessGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberGuessGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdba2ef99f0ab7e49f94a1104243273f382568d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberGuessGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberGuessGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
